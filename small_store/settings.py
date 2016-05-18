ad3109504ab3a2e89f4b6e760c441e919aa8656c
troviln <xomenko2@gmail.com>
2016-05-18 19:42:42 +0300

    last update

--- small_store/settings.py
+++ small_store/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'small_store.urls'
@@ -76,11 +77,22 @@ WSGI_APPLICATION = 'small_store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'troviln',
+        'PASSWORD': '66856ok',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
